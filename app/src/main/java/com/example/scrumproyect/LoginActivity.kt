cb388eaca24865d09ff3715410be62878b03e73e
Gerardo Flores <gerardo.gabriel.flores@gmail.com>
2019-10-14 15:10:42 -0500

    Gmail fix

--- app/src/main/java/com/example/scrumproyect/LoginActivity.kt
+++ app/src/main/java/com/example/scrumproyect/LoginActivity.kt
@@ -1,48 +1,48 @@
 package com.example.scrumproyect
 
-import android.annotation.SuppressLint
-import android.os.Bundle
-import org.json.JSONException
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
-import kotlinx.android.synthetic.main.activity_login.*
-import android.widget.Toast
 import android.content.Intent
 import android.util.Log
 import android.view.View
-import android.widget.ProgressBar
-import androidx.annotation.Nullable
+import android.widget.Toast
 import com.example.scrumproyect.view.presenter.UserPresenter
-import com.example.scrumproyect.view.ui.extensions.startActivity
 import com.example.scrumproyect.view.ui.base.ScrumBaseActivity
+import com.example.scrumproyect.view.ui.extensions.startActivity
 import com.example.scrumproyect.view.ui.utils.FacebookHelper
-import com.facebook.*
-import java.util.*
 import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
 import com.facebook.login.LoginResult
-import com.facebook.login.widget.LoginButton
-import com.google.firebase.auth.FacebookAuthProvider
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.*
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
-import io.paperdb.Paper
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_login.*
 import java.io.Serializable
-import kotlin.math.log
+import java.util.*
 
 class LoginActivity : ScrumBaseActivity(), UserPresenter.View {
 
     private val presenter = UserPresenter()
     private var callbackManager: CallbackManager? = null
-    /*private var loginButton: LoginButton? = null*/
     private var firebaseAuth: FirebaseAuth? = null
     private var firebaseAuthListener: FirebaseAuth.AuthStateListener? = null
+    private val googleApiClient: GoogleApiClient? = null
 
-    /*private var progressBar: ProgressBar? = null*/
+    val RC_SIGN_IN: Int = 1
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    lateinit var mGoogleSignInOptions: GoogleSignInOptions
+
 
     override fun getView() = R.layout.activity_login
 
     override fun onCreate() {
         super.onCreate()
         callbackManager = CallbackManager.Factory.create()
-        loginButton!!.setReadPermissions(Arrays.asList("email", "public_profile"))
+        loginButton!!.setReadPermissions(Arrays.asList("email"))
         login_test.setOnClickListener {
             presenter.login()
         }
@@ -74,6 +74,9 @@ class LoginActivity : ScrumBaseActivity(), UserPresenter.View {
             }
         }
 
+        configureGoogleSignIn()
+        setupUI()
+
     }
 
     override fun onResume() {
@@ -86,21 +89,6 @@ class LoginActivity : ScrumBaseActivity(), UserPresenter.View {
         presenter.detachView()
     }
 
-
-    private fun handleFacebookAccessToken(accessToken: AccessToken) {
-        progressBar!!.visibility = View.VISIBLE
-        loginButton!!.visibility = View.GONE
-
-        val credential = FacebookAuthProvider.getCredential(accessToken.token)
-        firebaseAuth!!.signInWithCredential(credential).addOnCompleteListener(this) { task ->
-            if (!task.isSuccessful) {
-                Toast.makeText(applicationContext, R.string.firebase_error_login, Toast.LENGTH_LONG).show()
-            }
-            progressBar!!.visibility = View.GONE
-            loginButton!!.visibility = View.VISIBLE
-        }
-    }
-
     private fun goMainScreen() {
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
@@ -109,7 +97,18 @@ class LoginActivity : ScrumBaseActivity(), UserPresenter.View {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        callbackManager!!.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                account?.let { firebaseAuthWithGoogle(it) }
+            } catch (e: ApiException) {
+                Toast.makeText(this, "Google sign in failed: onActivityResult ", Toast.LENGTH_LONG).show()
+            }
+        } else {
+            callbackManager!!.onActivityResult(requestCode, resultCode, data)
+        }
+
     }
 
     override fun onStart() {
@@ -125,5 +124,36 @@ class LoginActivity : ScrumBaseActivity(), UserPresenter.View {
     override fun successSchedule(flag: Int, vararg args: Serializable) {
         startActivity(MainActivity::class.java)
     }
+
+    private fun configureGoogleSignIn() {
+        mGoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, mGoogleSignInOptions)
+    }
+
+    private fun setupUI() {
+        google_button.setOnClickListener {
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        firebaseAuth!!.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+                startActivity(MainActivity::class.java)
+            } else {
+                Toast.makeText(this, "Google sign in failed firebaseAuthWithGoogle", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
 
 }
